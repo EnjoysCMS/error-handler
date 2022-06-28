7eae8c9c6af5a6bf2581e4b379f6d5bb53529067
Enjoys <deadly.info@gmail.com>
2022-06-28 14:38:49 +0300

    added catchErrors()

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace EnjoysCMS\ErrorHandler;
 
-use Doctrine\ORM\NoResultException;
-use EnjoysCMS\Core\Exception\ForbiddenException;
-use EnjoysCMS\Core\Exception\NotFoundException;
 use EnjoysCMS\ErrorHandler\Output\Html;
 use EnjoysCMS\ErrorHandler\Output\Image;
 use EnjoysCMS\ErrorHandler\Output\Json;
@@ -15,10 +12,8 @@ use EnjoysCMS\ErrorHandler\Output\Svg;
 use EnjoysCMS\ErrorHandler\Output\Xml;
 use HttpSoft\Emitter\EmitterInterface;
 use HttpSoft\Emitter\SapiEmitter;
-use HttpSoft\Message\Response;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 final class ErrorHandler implements ErrorHandlerInterface
 {
@@ -31,6 +26,24 @@ final class ErrorHandler implements ErrorHandlerInterface
         Image::class => ['image/gif', 'image/jpeg', 'image/png', 'image/webp']
     ];
 
+    private const ERROR_NAMES = [
+        E_ERROR => 'PHP Fatal Error',
+        E_WARNING => 'PHP Warning',
+        E_PARSE => 'PHP Parse Error',
+        E_NOTICE => 'PHP Notice',
+        E_CORE_ERROR => 'PHP Core Error',
+        E_CORE_WARNING => 'PHP Core Warning',
+        E_COMPILE_ERROR => 'PHP Compile Error',
+        E_COMPILE_WARNING => 'PHP Compile Warning',
+        E_USER_ERROR => 'PHP User Error',
+        E_USER_WARNING => 'PHP User Warning',
+        E_USER_NOTICE => 'PHP User Notice',
+        E_STRICT => 'PHP Strict Warning',
+        E_RECOVERABLE_ERROR => 'PHP Recoverable Error',
+        E_DEPRECATED => 'PHP Deprecated Warning',
+        E_USER_DEPRECATED => 'PHP User Deprecated Warning',
+    ];
+
     public const DEFAULT_STATUS_CODE = 500;
 
     private array $mappingCode = [];
@@ -39,22 +52,26 @@ final class ErrorHandler implements ErrorHandlerInterface
     private EmitterInterface $emitter;
     private LoggerInterface $logger;
     private ?Error $error = null;
-    private ?ServerRequestInterface $request = null;
 
 
-    public function __construct(LoggerInterface $logger = null, EmitterInterface $emitter = null)
-    {
+    public function __construct(
+        private ServerRequestInterface $request,
+        LoggerInterface $logger = null,
+        EmitterInterface $emitter = null
+    ) {
         $this->logger = $logger ?? new \Psr\Log\NullLogger();
         $this->emitter = $emitter ?? new SapiEmitter();
     }
 
-    public function handle(\Throwable $error, ServerRequestInterface $request): void
+    public function handle(\Throwable $error): void
     {
+        // disable error capturing to avoid recursive errors while handling exceptions
+        $this->unregister();
+
         $httpStatusCode = $this->getStatusCode($error);
         $this->sendToLogger($error);
 
-
-        $output = $this->getOutputProcessor($request);
+        $output = $this->getOutputProcessor();
         $this->emitter->emit(
             $output
                 ->setError($error)
@@ -62,20 +79,19 @@ final class ErrorHandler implements ErrorHandlerInterface
                 ->getResponse()
                 ->withStatus($httpStatusCode)
         );
+
         exit;
     }
 
-
-    private function getOutputProcessor(ServerRequestInterface $request)
+    private function getOutputProcessor()
     {
         foreach (self::PROCESSORS_MAP as $processor => $mimes) {
             foreach ($mimes as $mime) {
-                if (stripos($request->getHeaderLine('Accept'), $mime) !== false) {
+                if (stripos($this->request->getHeaderLine('Accept'), $mime) !== false) {
                     return new $processor($mime);
                 }
             }
         }
-
         return new Html();
     }
 
@@ -125,5 +141,92 @@ final class ErrorHandler implements ErrorHandlerInterface
     public function getErrorsMap(): array
     {
         return $this->errorsMap;
+    }
+
+    /**
+     * Catch Errors, Warning, etc
+     * @return void
+     * @throws \ErrorException
+     */
+    public function catchErrors()
+    {
+        $this->register();
+    }
+
+    /**
+     * Register this error handler.
+     */
+    public function register(): void
+    {
+        ini_set('display_errors', '0');
+
+        $logger = $this->logger;
+        // Handles throwable, echo output and exit.
+        set_exception_handler(function (\Throwable $error): void {
+            $this->handle($error);
+        });
+
+        // Handles PHP execution errors such as warnings and notices.
+        set_error_handler(static function (int $severity, string $message, string $file, int $line) use ($logger): bool {
+            if (!(error_reporting() & $severity)) {
+                // This error code is not included in error_reporting.
+                return true;
+            }
+
+            $error = new \ErrorException(
+                sprintf('%s: %s', self::ERROR_NAMES[$severity] ?? '', $message),
+                0,
+                $severity,
+                $file,
+                $line
+            );
+
+            if (self::isFatalError($severity)) {
+                throw $error;
+            }
+
+            $logger->debug($error->getMessage(), $error->getTrace());
+            return true;
+        });
+
+        // Handles fatal error.
+        register_shutdown_function(function (): void {
+            $e = error_get_last();
+
+            if ($e !== null && self::isFatalError($e['type'])) {
+                $error = new \ErrorException(
+                    sprintf('%s: %s', self::ERROR_NAMES[$e['type']] ?? '', $e['message']),
+                    0,
+                    $e['type'],
+                    $e['file'],
+                    $e['line']
+                );
+                dd($error);
+                $this->handle($error);
+            }
+        });
+    }
+
+    /**
+     * Unregisters this error handler by restoring the PHP error and exception handlers.
+     */
+    public function unregister(): void
+    {
+        restore_error_handler();
+        restore_exception_handler();
+    }
+
+
+    public static function isFatalError($severity)
+    {
+        return $severity > 0 && in_array($severity, [
+                E_ERROR,
+                E_PARSE,
+                E_CORE_ERROR,
+                E_CORE_WARNING,
+                E_COMPILE_ERROR,
+                E_COMPILE_WARNING,
+                E_USER_ERROR,
+            ], true);
     }
 }
