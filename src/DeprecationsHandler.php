1f8fe3abe6c08b931453c09738e96eecad9713d2
enjoys <deadly.info@gmail.com>
2023-09-10 22:26:03 +0300

    refactor HandlerDeprecations

--- src/DeprecationsHandler.php
+++ src/DeprecationsHandler.php
@@ -8,19 +8,26 @@ namespace EnjoysCMS\ErrorHandler;
 
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use ReflectionClass;
 use RuntimeException;
 
 final class DeprecationsHandler implements DeprecationsHandlerInterface
 {
-    private string $loggerType;
+    private string $loggerLevel = 'notice';
 
-    public function __construct(private LoggerInterface $logger, ?string $name = null, string $loggerType = 'notice')
+    public function __construct(private LoggerInterface $logger, ?string $loggerLevel = null, ?string $name = null)
+    {
+        $this->withName($name);
+        $this->setLoggerLevel($loggerLevel);
+    }
+
+    public function withName(?string $name): DeprecationsHandler
     {
         if ($this->logger instanceof Logger && $name) {
             $this->logger = $this->logger->withName($name);
         }
-        $this->loggerType = $loggerType;
+        return $this;
     }
 
     public function register(): void
@@ -28,7 +35,8 @@ final class DeprecationsHandler implements DeprecationsHandlerInterface
         set_error_handler(
             function (int $severity, string $message, string $file, int $line): bool {
                 if (in_array($severity, [E_USER_DEPRECATED, E_DEPRECATED], true)) {
-                    $this->logger->{$this->loggerType}(
+                    $this->logger->log(
+                        $this->loggerLevel,
                         sprintf(
                             'Deprecation: %s in %s on line %s',
                             $message,
@@ -42,28 +50,26 @@ final class DeprecationsHandler implements DeprecationsHandlerInterface
         );
     }
 
-    public function setLoggerType(string $method): DeprecationsHandler
+    public function setLoggerLevel(?string $level): DeprecationsHandler
     {
-        if (!method_exists($this->logger, $method) || $method === 'log') {
+        if ($level === null) {
+            return $this;
+        }
+
+        $logLevels = array_keys((new ReflectionClass(LogLevel::class))->getConstants());
+        if (!in_array(strtoupper($level), $logLevels, true)) {
             throw new RuntimeException(
                 sprintf(
                     '%s not allowed, allowed only (%s)',
-                    $method,
+                    $level,
                     implode(
                         ', ',
-                        array_filter(
-                            array_map(fn($item) => $item->name,
-                                (new ReflectionClass(LoggerInterface::class))->getMethods()
-                            ),
-                            function ($item) {
-                                return $item !== 'log';
-                            }
-                        )
+                        $logLevels
                     )
                 )
             );
         }
-        $this->loggerType = $method;
+        $this->loggerLevel = $level;
         return $this;
     }
 }
